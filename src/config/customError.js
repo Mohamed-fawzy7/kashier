7e4ae1fedfd739c284f55f90ab99f8f911d9ca7f
Fawzy <mohamed.fawzy7996@gmail.com>
2022-04-09 23:32:21 +0000

    add custom error

---
+++ src/config/customError.js
@@ -0,0 +1,19 @@
+class CustomError extends Error {
+    constructor(errorObject, statusCode) {
+        super('Custom error created');
+        this.name = 'CustomError';
+        this.errorObject = errorObject;
+        this.status = false;
+        this.statusCode = statusCode;
+    }
+
+    getErrorResponse() {
+        return {
+            status: this.status,
+            statusCode: this.statusCode,
+            errors: [this.errorObject]
+        };
+    }
+}
+
+module.exports = CustomError;
