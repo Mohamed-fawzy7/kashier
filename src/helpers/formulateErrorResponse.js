857d529076148df937c1e886b89986e417c68ad8
Fawzy <mohamed.fawzy7996@gmail.com>
2022-04-09 23:38:09 +0000

    add formulate error response helper

---
+++ src/helpers/formulateErrorResponse.js
@@ -0,0 +1,13 @@
+module.exports = error => {
+    if (error.name === 'CustomError') {
+        return error.getErrorResponse();
+    }
+
+    //should add logger to alert for errors not created on purpose
+    console.log(error);
+    return {
+        status: false,
+        statusCode: 500,
+        errors: [{ message: error.message }]
+    };
+};
