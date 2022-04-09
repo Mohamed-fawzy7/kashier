c86244be1b84615d7fcbf3b68931d0922b3ab2c8
Fawzy <mohamed.fawzy7996@gmail.com>
2022-04-09 23:08:50 +0000

    add db configuration

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+require("./src/config/db");
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const mongoConfig = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  keepAlive: true,
+};
+const mongoConnectionString = process.env.MONGO_CONNECTION_STRING;
+
+console.log({ mongoConnectionString });
+mongoose.connect(mongoConnectionString, mongoConfig);
+
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.error(err);
+});
+db.once("open", () => {
+  console.log("connected to db successfully");
+});
